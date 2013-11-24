82b1fb5498f504b9eae87638ba58b01888097e5e
Nyikos Zoltán <nyikoszoltan0@gmail.com>
2013-11-24 16:04:05 +0100

    add listadvertwindow

---
+++ src/listadvertwindow.h
@@ -0,0 +1,27 @@
+#ifndef _LISTADVERTWINDOW_H_
+#define _LISTADVERTWINDOW_H_
+#include "window.h"
+#include "advert.h"
+#include <vector>
+using namespace std;
+class ListAdvertWindow :Window {
+    vector<const Advert*> adverts;
+    public:
+    ListAdvertWindow(const vector<const Advert*>& adverts):Window("Adverts"), adverts(adverts){
+    }
+    virtual void handle() {
+        bool completed = false;
+        do{
+            drawTitle();
+            for(auto ad: adverts) {
+                cout << ad->getName() << endl;
+            }
+            cout << "(b)ack" << endl;
+            string cmd = readCommand("[b] > ");
+            if(cmd == "b") {
+                completed = true;
+            }
+        }while(!completed);
+    }
+};
+#endif
