05413ed89a61650211a926d0c3ee2bae272b66bd
Nyikos Zoltán <nyikoszoltan0@gmail.com>
2013-11-24 16:05:52 +0100

    Fix this

--- src/addadvertwindow.h
+++ src/addadvertwindow.h
@@ -2,13 +2,20 @@
 #define _ADDADVERTWINDOW_H_
 #include "advert.h"
 #include "window.h"
+#include "messagewindow.h"
 #include "newspaperselectionwindow.h"
 #include "adverttypeselectionwindow.h"
 class AddAdvertWindow:Window {
     Advert ad;
-    AddAdvertWindow():Window("Add advert"){}
-    bool cancelled;
+    vector<Newspaper*> newspapers;
+    const User* currentUser;
+    bool cancelled = false;
     public:
+    AddAdvertWindow(const vector<Newspaper>& newspapers, const User* currentUser):Window("Add advert"), currentUser(currentUser){
+        for(auto np : newspapers) {
+            this->newspapers.push_back(&np);
+        }
+    }
     Advert getAdvert() {
         return ad;
     }
@@ -16,15 +23,16 @@ class AddAdvertWindow:Window {
         return cancelled;
     }
     virtual void handle() {
+        drawTitle();
         bool complete = false;
         AdvertTypeSelectionWindow ts;
         ts.handle();
         string name;
         string image;
         string text;
-        stringstream cmdline;
-        vector<Newspaper*> newspapers;
+        vector<Newspaper*> selection;
         do {
+            stringstream cmdline;
             drawTitle();
             cout << "(n)ame: " << name << endl;
             cmdline << ",n";
@@ -32,28 +40,28 @@ class AddAdvertWindow:Window {
                 case AdvertType::Image: {
                     cout << "(i)mage: " << image << endl;
                     cmdline << ",i";
-                    if(image != "" && name != "" ) {
+                    if(image != "" && name != "" && selection.size()) {
                         cout << "(s)end" << endl;
                         cmdline << ",s";
                     }
-                };break
+                }break;
                 case AdvertType::Text: {
                     cout << "(t)ext: " << text << endl;
                     cmdline << ",t";
-                    if(text != "" && name != "") {
+                    if(text != "" && name != "" && selection.size()) {
                         cout << "(s)end" << endl;
                         cmdline << ",s";
                     }
-                };break
+                }break;
                 case AdvertType::TextImage: {
                     cout << "(t)ext: " << text << endl;
                     cout << "(i)mage: " << image << endl;
                     cmdline << ",t,i";
-                    if(text != "" && image != "" && name != "") {
+                    if(text != "" && image != "" && name != "" && selection.size()) {
                         cout << "(s)end" << endl;
                         cmdline << ",s";
                     }
-                };break
+                }break;
             }
             cout << "(s)elect (n)newspapers" << endl;
             cmdline << ",sn";
@@ -61,11 +69,16 @@ class AddAdvertWindow:Window {
             cmdline << ",c] > ";
             string prompt = cmdline.str();
             prompt[0]='[';
+            cout << selection.size() << endl;
             string cmd = readCommand(prompt);
             if(cmd == "n") {
                 name = readCommand("name > ");
-            if(cmd == "sn") {
-                NewspaperSelectionWindow sw
+            }else if(cmd == "sn") {
+                NewspaperSelectionWindow sw(newspapers,selection);
+                sw.handle();
+                if(!sw.isCancelled()) {
+                    selection = sw.getSelection();
+                }
             }else if(cmd == "i") {
                 image = readCommand("image > ");
             }else if(cmd == "t") {
@@ -73,24 +86,26 @@ class AddAdvertWindow:Window {
             }else if(cmd == "c") {
                 cancelled = true;
                 complete = true;
-            }else if(cmd == "s" && name != "") {
+            }else if(cmd == "s" && name != "" && selection.size() > 0) {
                 switch(ts.getType()) {
                     case AdvertType::Image: {
                         if(image != "") {
                             complete = true;
-                            ad = Advert::imageAdvert(name,newspapers,image);
+                            ad = Advert::imageAdvert(name,image,currentUser,selection);
                         }
-                    };break
+                    }break;
                     case AdvertType::Text: {
                         if(text != "") {
                             complete = true;
+                            ad = Advert::textAdvert(name,text,currentUser,selection);
                         }
-                    };break
+                    }break;
                     case AdvertType::TextImage: {
                         if(text != "" && image != "") {
+                            ad = Advert::textImageAdvert(name,text,image,currentUser,selection);
                             complete = true;
                         }
-                    };break
+                    }break;
                 }
             }
         }while(!complete);
